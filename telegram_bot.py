cd7f9dadf913badb41e55f6e1b7f8608b139956a
dexstroy <matej.rus7@gmail.com>
2023-01-02 20:51:19 +0100

    Action testing

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,6 +7,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+
 server_addr = 'http://django-service'
 
 def register(update, context):
